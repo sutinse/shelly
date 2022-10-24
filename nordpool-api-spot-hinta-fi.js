38fb4e28ebf7497796e82a7c76f9871c87b5da8c
sutinse <s.sutinen@cgi.com>
2022-10-24 10:58:02 +0300

    removed unnessary variable

--- nordpool-api-spot-hinta-fi.js
+++ nordpool-api-spot-hinta-fi.js
@@ -58,8 +58,6 @@ let pricesRankRetrived=0;
 let pricesPeriod=null;
 let previousRetrievedDate=0;
 let pricesPeriodRetrieved=0;
-// for String -> number conversion
-let clockHours=["00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","23","24"];
 
 // Retrive Ranked Prices
 function retrieveRankedPrices(url) {
